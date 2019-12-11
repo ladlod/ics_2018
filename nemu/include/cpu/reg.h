b0047a852977f250e7253263b67e4a3b3e3ee63c
ladlod <tian_zw@qq.com>
2019-12-12 03:24:20 +0800

    modify reg.h

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -3,9 +3,10 @@
 
 #include "common.h"
 
-enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
-enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+//寄存器
+enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };  //32位
+enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };  //16位
+enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };  //8位
 
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
@@ -15,18 +16,30 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
+
+
+  /*struct {
+    uint32_t _32;
+    uint16_t _16;
+    uint8_t _8[2];
+  } gpr[8];*/
+
+  /* Do NOT change the order of the GPRs' definitions. */
+union{
   struct {
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  };
+  union{
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
   } gpr[8];
-
-  /* Do NOT change the order of the GPRs' definitions. */
+};
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  //rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t eip;
 
