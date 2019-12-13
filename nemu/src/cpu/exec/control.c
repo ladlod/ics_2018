64357f33ce65aa4748c48e16d955502c955b0778
ladlod <tian_zw@qq.com>
2019-12-14 04:15:35 +0800

    complete call

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,7 +26,10 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
+  //TODO();
+
+  rtl_push(&(decoding.seq_eip));
+  rtl_j(decoding.jmp_eip);
 
   print_asm("call %x", decoding.jmp_eip);
 }
