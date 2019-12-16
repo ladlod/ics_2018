a0125741f09ec9aa400917cd08f8192ddd127d4e
tracer-ics2018 <tracer@njuics.org>
2019-12-16 23:14:37 +0800

    >  compile U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:14:37 up  2:17,  1 user,  load average: 0.44, 0.23, 0.14 f098ae7558da2e8e660fd6c6658f97f2d5f9ff9

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -29,7 +29,7 @@ static inline void idex(vaddr_t *eip, opcode_entry *e) {
 }
 
 static make_EHelper(2byte_esc);
-static make_EHelper(4byte_esc);
+//static make_EHelper(4byte_esc);
 
 #define make_group(name, item0, item1, item2, item3, item4, item5, item6, item7) \
   static opcode_entry concat(opcode_table_, name) [8] = { \
@@ -133,7 +133,7 @@ opcode_entry opcode_table [512] = {
   /* 0xe4 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe8 */	IDEX(J, call), IDEX(J, jmp), EMPTY, IDEXW(J, jmp, 1),
   /* 0xec */	IDEXW(in_dx2a, in, 1), EMPTY, IDEXW(out_a2dx, out, 1), EMPTY,
-  /* 0xf0 */	EMPTY, EMPTY, EMPTY, EX(4byte_esc),
+  /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY, //EX(4byte_esc),
   /* 0xf4 */	EMPTY, EMPTY, IDEXW(E, gp3, 1), IDEX(E, gp3),
   /* 0xf8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xfc */	EMPTY, EMPTY, IDEXW(E, gp4, 1), IDEX(E, gp5),
@@ -212,12 +212,12 @@ static make_EHelper(2byte_esc) {
   set_width(opcode_table[opcode].width);
   idex(eip, &opcode_table[opcode]);
 }
-static make_EHelper(4byte_esc) {
+/*static make_EHelper(4byte_esc) {
   uint32_t opcode = instr_fetch(eip, 3) | 0x100;
   decoding.opcode = opcode;
   set_width(opcode_table[opcode].width);
   idex(eip, &opcode_table[opcode]);
-}
+}*/
 
 make_EHelper(real) {
   uint32_t opcode = instr_fetch(eip, 1);
