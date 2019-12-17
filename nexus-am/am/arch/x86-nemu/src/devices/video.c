1c5d159f0bab53fdf990c875db2b3fc6bda55c02
tracer-ics2018 <tracer@njuics.org>
2019-12-17 15:19:29 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:19:29 up  2:35,  1 user,  load average: 0.21, 0.29, 0.18 e9cf97bc896aa516d0c0144076921b951f28f2aa

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -13,7 +13,7 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
       uint32_t screen_read;
       screen_read = inl(SCREEN_PORT);
-      printf("screen: %d\n", screen_read);
+      //printf("screen: %d\n", screen_read);
       info->width = screen_read >> 16;
       info->height = screen_read & 0xffff; 
       return sizeof(_VideoInfoReg);
@@ -26,12 +26,12 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      for(int i = 0; i < screen_height() ; i++){
+      /*for(int i = 0; i < screen_height() * screen_width(); i++){
         fb[i] = i;
-      }
-      /*for(int i = 0; i < ctl->h; i++){
-        memcpy(fb + (ctl->y + i)*screen_width() + ctl->x, ctl->pixels + i*ctl->w, ctl->w*4);
       }*/
+      for(int i = 0; i < ctl->h; i++){
+        memcpy(fb + (ctl->y + i)*screen_width() + ctl->x, ctl->pixels + i*ctl->w, ctl->w*4);
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
