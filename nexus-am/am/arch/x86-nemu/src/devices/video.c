d2210139cfd034451024dccf443abbf8527ce332
tracer-ics2018 <tracer@njuics.org>
2019-12-17 16:15:44 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:15:44 up  3:31,  1 user,  load average: 0.04, 0.01, 0.02 36f97f707d32133bd04a9f39bc5fde1166b494d6

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -14,7 +14,7 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
       uint32_t screen_read;
       screen_read = inl(SCREEN_PORT);
       //printf("screen: %d\n", screen_read);
-      info->width = screen_read; //>> 16;
+      info->width = screen_read & 0xffff0000; //>> 16;
       info->height = screen_read & 0xffff; 
       return sizeof(_VideoInfoReg);
     }
