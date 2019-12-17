0984e6805ae4ece36c8b8c9a676b97ed9ab00559
tracer-ics2018 <tracer@njuics.org>
2019-12-17 15:05:27 +0800

    >  compile U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:05:27 up  2:21,  1 user,  load average: 0.03, 0.13, 0.10 465906d3f2430215d0771bf0b263f6ad2bbe0d06

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,16 +13,18 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  if(is_mmio(addr) != -1){
-    return mmio_read(addr, len, is_mmio(addr));
+  int m = is_mmio(addr);
+  if(m != -1){
+    return mmio_read(addr, len, m);
   }
   else
     return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
-  if(is_mmio(addr) != -1){
-    mmio_write(addr, len, data, is_mmio(addr));
+  int m = is_mmio(addr);
+  if(m != -1){
+    mmio_write(addr, len, data, m);
   }
   else
     memcpy(guest_to_host(addr), &data, len);
