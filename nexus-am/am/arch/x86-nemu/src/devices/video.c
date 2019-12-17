1f0169dc8d6483ee00c240f47407081cc8150699
tracer-ics2018 <tracer@njuics.org>
2019-12-17 15:17:45 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:17:45 up  2:33,  1 user,  load average: 0.41, 0.28, 0.16 431f05be9ba7c22396ac6618f3b357b87ba73ae3

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -14,7 +14,7 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
       uint32_t screen_read;
       screen_read = inl(SCREEN_PORT);
       printf("screen: %d\n", screen_read);
-      info->width = screen_read & 0xffff0000 >> 16;
+      info->width = screen_read >> 16;
       info->height = screen_read & 0xffff; 
       return sizeof(_VideoInfoReg);
     }
@@ -26,7 +26,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      for(int i = 0; i < screen_height() * screen_width(); i++){
+      for(int i = 0; i < screen_height() ; i++){
         fb[i] = i;
       }
       /*for(int i = 0; i < ctl->h; i++){
