cdb15a2b15e7fdd8857f838a469ad55fbb52b723
tracer-ics2018 <tracer@njuics.org>
2019-12-17 11:48:49 +0800

    >  compile U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:48:49 up  3:43,  1 user,  load average: 0.04, 0.07, 0.02 1aa05c9de2b46c56656f29a39f49ee3f2588a81

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,7 +13,7 @@ uint8_t pmem[PMEM_SIZE];
 
 uint32_t paddr_read(paddr_t addr, int len) {
   //is_mmio()
-  printf("0x%08x\n", addr);
+  //printf("0x%08x\n", addr);
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
