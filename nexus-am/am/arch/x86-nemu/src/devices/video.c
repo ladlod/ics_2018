85ea0d6972a7e4b8e54a1aa3766ae8667552fa3a
tracer-ics2018 <tracer@njuics.org>
2019-12-17 17:03:20 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:03:20 up  4:19,  1 user,  load average: 0.16, 0.07, 0.03 55975c8f88c5bc7286a73f354867d7f4314e9b97

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,7 +3,7 @@
 #include <amdev.h>
 #include <klib.h>
 
-#define SCREEN_PORT 0x40000
+#define SCREEN_PORT 0x100
 
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000; //内存映射起始地址
 
@@ -26,15 +26,15 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      int size = screen_height() * screen_width();
-      //printf("size=%d", size);
+      /*int size = screen_height() * screen_width();
+      printf("size=%d", size);
       for(int i = 0; i < size; i++){
         fb[i] = i;
-      }
-      /*
+      }*/
+      
       for(int i = 0; i < ctl->h; i++){
         memcpy(fb + (ctl->y + i)*screen_width() + ctl->x, ctl->pixels + i*ctl->w, ctl->w*4);
-      }*/
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
