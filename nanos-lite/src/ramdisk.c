e3088b07341a116b44bb229ca769e71f40522720
tracer-ics2018 <tracer@njuics.org>
2019-12-18 16:22:47 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:22:47 up 31 min,  1 user,  load average: 0.12, 0.18, 0.32 b1f64f54fa944a171b2c43a7858c5cec6a8a09d

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,7 +12,7 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  printf("ramdisk_start: 0x%x, offset = 0x%x len=0x%x\n", ramdisk_start, offset, len);
+  printf("ramdisk_start: %d, offset = %d len=%d\n", ramdisk_start, offset, len);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
 }
