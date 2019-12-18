d74849029ff7d885b36cd71f0963514d92797612
tracer-ics2018 <tracer@njuics.org>
2019-12-18 11:49:20 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:49:20 up  2:55,  1 user,  load average: 0.03, 0.02, 0.01 47e9803ffcd75d31638a42a75d52b0c87b615ba2

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -11,7 +11,7 @@ _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
   if (user_handler) {
     printf("test1\n");
-    _Event ev = {0};
+    _Event ev;
     printf("test2\n");
     printf("irq = 0x%x\n", tf->irq);
     switch (tf->irq) {
