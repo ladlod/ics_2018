655ddb55ce68ecb859d70a14eaebc9882e90175f
tracer-ics2018 <tracer@njuics.org>
2019-12-25 07:53:12 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  07:53:12 up 19 min,  1 user,  load average: 0.00, 0.07, 0.13 8e50dcdc1157022ee0da3ddd593258559d249ae7

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,10 +7,6 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  //printf("test");
-  //int fd = open(filename, 0, 0);
-  //size_t len = get_ramdisk_size() / 8;
-  //printf("len = %d\n", len);
   ramdisk_read((void *)DEFAULT_ENTRY, 0, get_ramdisk_size());
   Log("done ramdisk read");
   //TODO();
