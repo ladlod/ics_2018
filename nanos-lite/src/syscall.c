e77fc18a6e73fefc005fa1cf3ac7c853c7480b50
tracer-ics2018 <tracer@njuics.org>
2019-12-25 18:14:58 +0800

    >  run U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:14:58 up 40 min,  1 user,  load average: 0.00, 0.00, 0.00 2993ad981f5187ac52223aaf80606f007b2f6164

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,7 @@ _Context* do_syscall(_Context *c) {
   //printf("a0=%d\n", a[0]);
 
   switch (a[0]) {
-    case SYS_exit: _halt(a[2]); break;
+    case SYS_exit: _halt(a[3]); break;
     case SYS_yield: _yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
