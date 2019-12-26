af022e07fa62f5e02fda399f192b0eafa7f683ba
tracer-ics2018 <tracer@njuics.org>
2019-12-26 13:58:11 +0800

    >  compile U201614526 root Linux ladlod-Ubuntu1 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:58:11 up 35 min,  1 user,  load average: 0.03, 0.05, 0.07 88fc879ad55f5599c34c3ad1af484af7f03bc44

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -36,7 +36,7 @@ make_EHelper(mov_cr2r) {
 
 make_EHelper(int) {
   //TODO();
-  printf("\nint 0x%x", id_dest->val);
+  //printf("\nint 0x%x", id_dest->val);
   raise_intr(id_dest->val, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
